52b4ab58511134958d892b9a8214296f6791f2ff
Rosamon <klimroman2001@mail.ru>
2023-12-11 21:58:57 +0300

    Implemented preventing IDOR

--- resources_idor.py
+++ resources_idor.py
@@ -8,15 +8,19 @@ from flask_login import login_required, current_user
 @app.route("/idor")
 @login_required
 def redirect_to_page():
-    found_id = (UserModel.find_by_username(current_user.username)).id
+    found_id = current_user.id
     # находим в бд пользователя текущей сессии и переходим на страницу с его id
     return redirect('/idor/'+str(found_id))
 
 @app.route("/idor/<id>")
 @login_required
 def index_idor(id):
-    #username = current_user.username
+    
+    current_id = escape(id)
+    if int(current_user.id) != int(current_id):
+        flash(f'You have not access to these data!')
+        return redirect('/idor/'+str(current_user.id))
 
-    user = UserModel.find_by_id(escape(id))
+    user = UserModel.find_by_id(escape(current_id))
 
     return make_response(render_template('idor.html', secret=user.secret, username = user.username), 200)
